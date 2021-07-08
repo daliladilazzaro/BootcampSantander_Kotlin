877a29310611c749be7346c7183f1e2d19221bcc
daliladilazzaro <dilazzaro06@gmail.com>
2021-07-08 16:56:56 -0300

    projeto novo

---
+++ src/main/kotlin/bank/Pessoa.kt
@@ -0,0 +1,14 @@
+package bank
+
+class Pessoa {
+    var nome:String="Lila"
+    var cpf:String="123.456.789.90"
+
+}
+
+fun main (){
+    val lila = Pessoa()
+
+    print(lila.nome)
+    print(lila.cpf)
+}
