e98de5960b53c282eb62bbb793a38060ff16f1ee
daliladilazzaro <dilazzaro06@gmail.com>
2021-07-08 21:53:57 -0300

    inclusao de nova variavel para teste

--- src/main/kotlin/bank/Pessoa.kt
+++ src/main/kotlin/bank/Pessoa.kt
@@ -3,12 +3,16 @@ package bank
 class Pessoa {
     var nome:String="Lila"
     var cpf:String="123.456.789.90"
-
+private set
+    inner class Endereco{
+        var rua:String="Rua ruarua"
+    }
 }
 
 fun main (){
     val lila = Pessoa()
+    println(lila.nome)
+    println(lila.cpf)
 
-    print(lila.nome)
-    print(lila.cpf)
+    println(lila.Endereco().rua)
 }
